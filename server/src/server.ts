0d7af7a68ed56bdf520edcceb25b72056333f61d
Gabriel Peruchi <gabriel-peruchi@hotmail.com>
2022-09-16 22:13:04 -0300

    feat(server): prisma config and database schema

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,22 @@ import express from 'express'
 
 const app = express()
 
+app.get('/games', (request, response) => {
+  return response.json([])
+})
+
+app.get('/games/:gameId/ads', (request, response) => {
+  return response.json([])
+})
+
+app.post('/ads', (request, response) => {
+  return response.status(201).json({})
+})
+
+app.get('/ads/:adId/discord', (request, response) => {
+  return response.json({})
+})
+
 app.listen(3333, () => {
   console.log('Server is running on port 3333')
 })
